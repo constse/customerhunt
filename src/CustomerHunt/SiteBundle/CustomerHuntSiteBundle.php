48a12e0ea2018377cd5cf1eac949798452d354c8
Constantine Seleznyoff <const.seoff@gmail.com>
2015-03-17 17:57:14 +0600

    Добавлены основные бандлы

---
+++ src/CustomerHunt/SiteBundle/CustomerHuntSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CustomerHunt\SiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CustomerHuntSiteBundle extends Bundle
+{
+}
---
+++ src/CustomerHunt/SystemBundle/CustomerHuntSystemBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CustomerHunt\SystemBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CustomerHuntSystemBundle extends Bundle
+{
+}
